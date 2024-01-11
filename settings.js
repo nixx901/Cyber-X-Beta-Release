6190dfa110703389c2f3e522d75485339a8c1705
nixx901 <137382002+nixx901@users.noreply.github.com>
2024-01-11 20:25:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ global.APIs = {
 
 //API KEY\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'your key',
+	'https://zenzapis.xyz': 'nnq8k4ebu2qbht9qr22s4gp43wc5snbmencvf5b6odhrosckv5z1p3efky7f1l1q',
 }
 
 
@@ -36,25 +36,25 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['916001031685'] //ur owner number
+global.ownername = "ᴋɪɴɢ PRIME ᴀʟᴘʜᴀ" //ur owner name
+global.ytname = "YT: No Yt" //ur yt chanel name
+global.socialm = "GitHub: Nixx69" //ur github or insta name
+global.location = "Kamalput, Baihata, Kokeriya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.owner = ['916001031685']
+global.ownertag = '916001031685' //ur tag number
+global.botnumber = '916001031685' //Add your Bot Number For Log
+global.botname = 'Prime-AI BETA EDITION' //ur bot name
+global.ig = "⦸ _This message was deleted_" //your theme url which will be displayed on whatsapp
+global.myweb = "⦸ _This message was deleted_" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.linkz = '⦸ _This message was deleted_'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
+global.packname = "Prime-AI Test Bot" //ur sticker watermark packname
+global.author = "Prime ALPHA" //ur sticker watermark author
 global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['916001031685'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
